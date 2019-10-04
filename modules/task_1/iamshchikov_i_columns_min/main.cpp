be024d1be14789d4976d2bf6edbe0175c5cf64c2
Ivan Iamshchikov <www.vano565@gmail.com>
2019-10-04 17:15:32 +0300

    fix code style

--- modules/task_1/iamshchikov_i_columns_min/main.cpp
+++ modules/task_1/iamshchikov_i_columns_min/main.cpp
@@ -1,31 +1,37 @@
 // Copyright 2019 Iamshchikov Ivan
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
+#include <vector>
 #include "./columns_min.h"
 
 TEST(Parallel_Operations_MPI, cant_create_matrix_with_negative_scale) {
     ASSERT_ANY_THROW(std::vector <std::vector<int> > matrix(-1));
     std::vector <std::vector<int> > matrix(3);
-    ASSERT_ANY_THROW(getRandomMatrix(matrix, -5));
+    std::vector <std::vector<int> >* pmatrix = &matrix;
+    ASSERT_ANY_THROW(getRandomMatrix(pmatrix, -5));
 }
 
 TEST(Parallel_Operations_MPI, can_create_matrix) {
+    size_t m = 3, n = 5;
     std::vector <std::vector<int> > matrix(3);
-    getRandomMatrix(matrix, 5);
-    ASSERT_EQ(3, matrix.size());
-    ASSERT_EQ(5, matrix[0].size());
+    std::vector <std::vector<int> >* pmatrix = &matrix;
+    getRandomMatrix(pmatrix, n);
+    ASSERT_EQ(m, pmatrix->size());
+    ASSERT_EQ(n, pmatrix->at(0).size());
 }
 
 TEST(Parallel_Operations_MPI, can_transform_matrix_to_row) {
     int k = 0;
     const int m = 5, n = 6;
     std::vector <std::vector<int> > matrix(m);
+    std::vector <std::vector<int> >* pmatrix = &matrix;
     std::vector<int> matrix_in_row(m*n);
-    getRandomMatrix(matrix, n);
-    matrixToRow(matrix, matrix_in_row, m, n);
-    for (int i = 0;i < n;++i)
-        for (int j = 0;j < m;++j)
-            ASSERT_EQ(matrix[j][i], matrix_in_row[k++]);
+    std::vector<int>* pmatrix_in_row = &matrix_in_row;
+    getRandomMatrix(pmatrix, n);
+    matrixToRow(pmatrix, pmatrix_in_row, m, n);
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            ASSERT_EQ(pmatrix->at(j)[i], pmatrix_in_row->at(k++));
 }
 
 TEST(Parallel_Operations_MPI, find_min_in_row) {
@@ -34,9 +40,10 @@ TEST(Parallel_Operations_MPI, find_min_in_row) {
     const int m = 1, n = 6;
     if (rank == 0) {
         std::vector <std::vector<int> > matrix(m);
-        getRandomMatrix(matrix, n);
-        ASSERT_EQ(getMinElementsSequential(matrix, m, n),
-            getMinElementsParallel(matrix, m, n));
+        std::vector <std::vector<int> >* pmatrix = &matrix;
+        getRandomMatrix(pmatrix, n);
+        ASSERT_EQ(getMinElementsSequential(pmatrix, m, n),
+                  getMinElementsParallel(pmatrix, m, n));
     }
 }
 
@@ -46,9 +53,10 @@ TEST(Parallel_Operations_MPI, find_min_in_col) {
     const int m = 5, n = 1;
     if (rank == 0) {
         std::vector <std::vector<int> > matrix(m);
-        getRandomMatrix(matrix, n);
-        ASSERT_EQ(getMinElementsSequential(matrix, m, n),
-            getMinElementsParallel(matrix, m, n));
+        std::vector <std::vector<int> >* pmatrix = &matrix;
+        getRandomMatrix(pmatrix, n);
+        ASSERT_EQ(getMinElementsSequential(pmatrix, m, n),
+                  getMinElementsParallel(pmatrix, m, n));
     }
 }
 
@@ -58,9 +66,10 @@ TEST(Parallel_Operations_MPI, find_mins_in_matrix) {
     const int m = 5, n = 6;
     if (rank == 0) {
         std::vector <std::vector<int> > matrix(m);
-        getRandomMatrix(matrix, n);
-        ASSERT_EQ(getMinElementsSequential(matrix, m, n),
-            getMinElementsParallel(matrix, m, n));
+        std::vector <std::vector<int> >* pmatrix = &matrix;
+        getRandomMatrix(pmatrix, n);
+        ASSERT_EQ(getMinElementsSequential(pmatrix, m, n),
+                  getMinElementsParallel(pmatrix, m, n));
     }
 }
 
