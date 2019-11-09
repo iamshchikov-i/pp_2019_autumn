9af87fa8228012f00e4606822b23f63fb562300b
Ivan Iamshchikov <www.vano565@gmail.com>
2019-11-09 11:48:13 +0300

    fix after review

--- modules/task_1/iamshchikov_i_columns_min/main.cpp
+++ modules/task_1/iamshchikov_i_columns_min/main.cpp
@@ -5,94 +5,94 @@
 #include "./columns_min.h"
 
 TEST(Parallel_Operations_MPI, cant_create_matrix_with_negative_scale) {
-    ASSERT_ANY_THROW(std::vector <std::vector<int> > matrix(-1));
-    std::vector <std::vector<int> > matrix(3);
-    std::vector <std::vector<int> >* pmatrix = &matrix;
-    ASSERT_ANY_THROW(getRandomMatrix(pmatrix, -5));
+	ASSERT_ANY_THROW(std::vector <std::vector<int> > matrix(-1));
+	size_t m = 3, n = -5;
+	std::vector <int> matrix;
+	std::vector <int>* pmatrix = &matrix;
+	ASSERT_ANY_THROW(getRandomMatrix(pmatrix, n, m));
 }
 
 TEST(Parallel_Operations_MPI, can_create_matrix) {
-    size_t m = 3, n = 5;
-    std::vector <std::vector<int> > matrix(3);
-    std::vector <std::vector<int> >* pmatrix = &matrix;
-    getRandomMatrix(pmatrix, n);
-    ASSERT_EQ(m, pmatrix->size());
-    ASSERT_EQ(n, pmatrix->at(0).size());
+	size_t m = 3, n = 5;
+	std::vector <int> matrix;
+	std::vector <int>* pmatrix = &matrix;
+	getRandomMatrix(pmatrix, n, m);
+	ASSERT_EQ(m*n, pmatrix->size());
 }
 
 TEST(Parallel_Operations_MPI, can_transform_matrix_to_row) {
-    int k = 0;
-    const int m = 5, n = 6;
-    std::vector <std::vector<int> > matrix(m);
-    std::vector <std::vector<int> >* pmatrix = &matrix;
-    std::vector<int> matrix_in_row(m*n);
-    std::vector<int>* pmatrix_in_row = &matrix_in_row;
-    getRandomMatrix(pmatrix, n);
-    matrixToRow(pmatrix, pmatrix_in_row, m, n);
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < m; ++j)
-            ASSERT_EQ(pmatrix->at(j)[i], pmatrix_in_row->at(k++));
+	int k = 0;
+	const int m = 5, n = 6;
+	std::vector <int> matrix;
+	std::vector <int>* pmatrix = &matrix;
+	std::vector<int> transposed_matrix(m*n);
+	std::vector<int>* ptransposed_matrix = &transposed_matrix;
+	getRandomMatrix(pmatrix, n, m);
+	transposeMatrix(pmatrix, ptransposed_matrix, m, n);
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
+			ASSERT_EQ(pmatrix->at(j*n + i), ptransposed_matrix->at(k++));
 }
 
 TEST(Parallel_Operations_MPI, find_min_in_row) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    const int m = 1, n = 6;
-    std::vector<int> res;
-    std::vector <std::vector<int> > matrix(m);
-    std::vector <std::vector<int> >* pmatrix = &matrix;
-    if (rank == 0) {
-        getRandomMatrix(pmatrix, n);
-    }
-    res = getMinElementsParallel(pmatrix, m, n);
-    if (rank == 0) {
-        ASSERT_EQ(getMinElementsSequential(pmatrix, m, n), res);
-    }
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	const int m = 1, n = 6;
+	std::vector<int> res;
+	std::vector <int> matrix;
+	std::vector <int>* pmatrix = &matrix;
+	if (rank == 0) {
+		getRandomMatrix(pmatrix, n, m);
+	}
+	res = getMinElementsParallel(pmatrix, m, n);
+	if (rank == 0) {
+		ASSERT_EQ(getMinElementsSequential(pmatrix, m, n), res);
+	}
 }
 
 TEST(Parallel_Operations_MPI, find_min_in_col) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    const int m = 6, n = 1;
-    std::vector<int> res;
-    std::vector <std::vector<int> > matrix(m);
-    std::vector <std::vector<int> >* pmatrix = &matrix;
-    if (rank == 0) {
-        getRandomMatrix(pmatrix, n);
-    }
-    res = getMinElementsParallel(pmatrix, m, n);
-    if (rank == 0) {
-        ASSERT_EQ(getMinElementsSequential(pmatrix, m, n), res);
-    }
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	const int m = 6, n = 1;
+	std::vector<int> res;
+	std::vector <int> matrix;
+	std::vector <int>* pmatrix = &matrix;
+	if (rank == 0) {
+		getRandomMatrix(pmatrix, n, m);
+	}
+	res = getMinElementsParallel(pmatrix, m, n);
+	if (rank == 0) {
+		ASSERT_EQ(getMinElementsSequential(pmatrix, m, n), res);
+	}
 }
 
 TEST(Parallel_Operations_MPI, find_mins_in_matrix) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    const int m = 5, n = 6;
-    std::vector<int> res;
-    std::vector <std::vector<int> > matrix(m);
-    std::vector <std::vector<int> >* pmatrix = &matrix;
-    if (rank == 0) {
-        getRandomMatrix(pmatrix, n);
-    }
-    res = getMinElementsParallel(pmatrix, m, n);
-    if (rank == 0) {
-        ASSERT_EQ(getMinElementsSequential(pmatrix, m, n), res);
-    }
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	const int m = 1000, n = 1000;
+	std::vector<int> res;
+	std::vector <int> matrix;
+	std::vector <int>* pmatrix = &matrix;
+	if (rank == 0) {
+		getRandomMatrix(pmatrix, n, m);
+	}
+	res = getMinElementsParallel(pmatrix, m, n);
+	if (rank == 0) {
+		ASSERT_EQ(getMinElementsSequential(pmatrix, m, n), res);
+	}
 }
 
 int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    MPI_Init(&argc, &argv);
+	::testing::InitGoogleTest(&argc, argv);
+	MPI_Init(&argc, &argv);
 
-    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-    ::testing::TestEventListeners& listeners =
-        ::testing::UnitTest::GetInstance()->listeners();
+	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+	::testing::TestEventListeners& listeners =
+		::testing::UnitTest::GetInstance()->listeners();
 
-    listeners.Release(listeners.default_result_printer());
-    listeners.Release(listeners.default_xml_generator());
+	listeners.Release(listeners.default_result_printer());
+	listeners.Release(listeners.default_xml_generator());
 
-    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-    return RUN_ALL_TESTS();
+	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+	return RUN_ALL_TESTS();
 }
