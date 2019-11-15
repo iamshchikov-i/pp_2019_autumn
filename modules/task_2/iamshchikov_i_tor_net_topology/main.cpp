6168da1d858bbdc04579298567f7fb379213550d
Ivan Iamshchikov <www.vano565@gmail.com>
2019-11-15 10:31:28 +0300

    fix test

--- modules/task_2/iamshchikov_i_tor_net_topology/main.cpp
+++ modules/task_2/iamshchikov_i_tor_net_topology/main.cpp
@@ -139,7 +139,7 @@ TEST(torus_network_topology, can_send) {
 
         if (procrank == dest) {
             ASSERT_EQ(count, pdata->size());
-            for (int i = 0; i < count; ++i)
+            for (size_t i = 0; i < count; ++i)
                 ASSERT_EQ(i*i, pdata->at(i));
         }
     }
