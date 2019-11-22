f7fce6deb3032c13b1c97ab7d28d313faedd728a
Ivan Iamshchikov <www.vano565@gmail.com>
2019-11-22 16:39:42 +0300

    minimizer implementation

---
+++ modules/task_3/iamshchikov_i_minimizer/functions.cpp
@@ -0,0 +1,13 @@
+#include "functions.h"
+
+double f1(double x, double y) {
+	return pow(x, 2) + pow(y - 1, 2);
+}
+
+double f2(double x, double y) {
+	return pow(x, 2) - x * y + pow(y, 2) - 2 * x + y;
+}
+
+double f3(double x, double y) {
+	return pow(x, 2) + x * y + pow(y, 2) - 4 * log(x) - 10 * log(y);
+}
---
+++ modules/task_3/iamshchikov_i_minimizer/functions.h
@@ -0,0 +1,10 @@
+#ifndef MODULES_TASK_3_IAMSHCHIKOV_I_FUNCTIONS_H__
+#define MODULES_TASK_3_IAMSHCHIKOV_I_FUNCTIONS_H__
+
+#include <cmath>
+
+double f1(double x, double y);
+double f2(double x, double y);
+double f3(double x, double y);
+
+#endif #endif // MODULES_TASK_3_IAMSHCHIKOV_I_FUNCTIONS_H__
